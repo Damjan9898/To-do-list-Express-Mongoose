e50f69ace213275a2b2dc2286a7028b5943efc1c
unknown <damjanbelojevic98.db@gmail.com>
2023-02-12 20:17:19 +0100

    Adding database

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
-//Pristup drugom js fajlu
-const date = require(__dirname + '/date.js');
+//Kreiram novu Bazu
+mongoose.connect("mongodb://localhost:27017/todoDB", { useNewUrlParser: true });
 
 const app = express();
 
@@ -13,81 +15,231 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-let newTodos= ["Buy food", "Cook food", "Eat food"];
 
-let newWorks = ["School", "Homework", "Job"];
+const itemSchema = new mongoose.Schema({
+    name: String
+})
 
-let checkedItem = []
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemSchema]
+})
+
+const Item = mongoose.model("Item", itemSchema);
+
+const List = mongoose.model("List", listSchema);
+
+// Kreiranje default itema
+const item1 = new Item({
+    name: "Welcome to your todo list"
+});
+const item2 = new Item({
+    name: "Hit the + button to add new item"
+});
+const item3 = new Item({
+    name: "<-- Click this to delete an item"
+});
+
+
 
 app.get("/", (req, res)=>{
 
-    let currentDate = date.getFullDate();
+    let allListNames = [];
 
-    res.render('index', {listTitle: currentDate, newListItems: newTodos})
+    List.find((err, items)=>{
+        if(!err){
+            items.forEach((item)=>{
+                if(item.name != "Favicon.ico"){
+                    allListNames.push(item.name);
+                }
+
+            })
+        }
+    })
+
+
+
+    Item.find((err, items)=>{
+        if(err){
+            console.log(err)
+        }else{
+
+            //U slucaju da je prazna baza dodajem default vrednosti
+            if(items.length === 0){
+                //Dodavanje default itema u bazu u kolekciju Items
+                Item.insertMany([item1, item2, item3], (err)=>{
+                    if(err){
+                        console.log(err)
+                    }else{
+                        console.log("Success insert many")
+                    }
+                });
+
+                res.redirect("/");
+
+            }
+            res.render('index', {listTitle: "Today", newListItems: items, allListNames: allListNames})
+        }
+    });
+
+    
 
     
 })
 
-
-app.get("/work", (req, res)=>{
-
-    res.render('index', {listTitle: "Work list", newListItems: newWorks})
-
-})
-
-
-
 app.get("/about", (req, res)=>{
 
-    res.render('about')
+    let allListNames = [];
+
+    List.find((err, items)=>{
+        if(!err){
+            items.forEach((item)=>{
+                if(item.name != "Favicon.ico"){
+                    allListNames.push(item.name);
+                }
+
+            })
+            res.render('about', {allListNames: allListNames})
+        }
+    })
+
+    
 
     
 })
 
 app.post("/", (req, res)=>{
 
+    let listTitle = req.body.listButton;
+
     if(req.body.item != ""){
 
         let item = req.body.item;
 
-        if(req.body.list === "Work"){
+        const newItem = new Item({
+            name: item
+        })
 
-            newWorks.push(item)
-            res.redirect("/work")
-
+        if(listTitle === "Today"){
+            newItem.save();
+            res.redirect("/");
         }else{
-
-            newTodos.push(item)
-            res.redirect("/")
+            List.findOne({name: listTitle}, (err, foundList)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    foundList.items.push(newItem);
+                    foundList.save();
+                    res.redirect("/" + listTitle);
+                }
+            })
         }
-
     }else{
-        if(req.body.list === "Work"){
-            res.redirect("/work")
-
-        }else{
-            res.redirect("/")
-        }
+        res.redirect("/" + listTitle);
     }
 
     
 })
 
-app.post("/work", (req, res)=>{
 
-    if(req.body.item != ""){
-        newWork = req.body.item;
 
-        newWorks.push(newWork)
+app.get("/:customListName", (req,res)=>{
 
-        res.redirect("/work")
+
+    let allListNames = [];
+
+    List.find((err, items)=>{
+        if(!err){
+            items.forEach((item)=>{
+                //Ovo je default dodato jer je los primer
+                if(item.name != "Favicon.ico"){
+                    allListNames.push(item.name);
+                }
+                
+            })
+        }
+    })
+
+
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({name: customListName}, (err, customList)=>{
+        if(err){
+            console.log(err)
+        }else{
+
+            if(!customList){
+
+                const list = new List({
+                    name: customListName,
+                    items: [item1, item2, item3]
+                })
+
+                list.save();
+
+                res.render("index", {listTitle: list.name, newListItems: list.items, allListNames:allListNames})
+
+
+            }else{
+                res.render("index", {listTitle: customList.name, newListItems: customList.items, allListNames:allListNames})
+            }
+
+            
+        }
+    })
+
+
     
+});
+
+
+app.post("/delete", (req, res)=>{
+
+    let selectedItemId = req.body.checkbox;
+
+    let hiddenListName = req.body.hiddenListName;
+
+    if(hiddenListName === "Today"){
+
+    Item.findByIdAndRemove(selectedItemId, (err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Success delete items");
+            res.redirect("/")
+        }
+    });
+
     }else{
-        res.redirect("/work")
+
+        List.findOneAndUpdate({name: hiddenListName}, {$pull: {items: {_id:selectedItemId}}}, (err, item)=>{
+            if(!err){
+                res.redirect("/" + hiddenListName);
+            }
+        });
+
     }
-    
-    
+
+});
+
+
+
+app.post("/newList", (req, res)=>{
+
+    let listTitle = _.capitalize(req.body.newListName);
+
+    res.redirect("/" + listTitle);
+
 })
+
+app.post("/getList", (req, res)=>{
+
+    let getListName = req.body.getListName;
+
+    res.redirect("/" + getListName);
+
+})
+
 
 app.listen("3000", ()=>{
     console.log("Listen on port 3000")
